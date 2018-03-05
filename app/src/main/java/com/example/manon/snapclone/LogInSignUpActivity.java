284943de49e06554600a47fbb5e8d462bc0dd111
Manon Philippot <33070721+maphdev@users.noreply.github.com>
2018-03-05 22:51:54 +0100

    Login activity and layout.

--- app/src/main/java/com/example/manon/snapclone/LogInSignUpActivity.java
+++ app/src/main/java/com/example/manon/snapclone/LogInSignUpActivity.java
@@ -1,13 +1,36 @@
 package com.example.manon.snapclone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInSignUpActivity extends AppCompatActivity {
+
+    private Button connexionButton;
+    private Button inscriptionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_signup);
+
+        final Intent i = new Intent(this, LoginActivity.class);
+
+        final int resultCode = 0;
+        connexionButton = findViewById(R.id.connexionButton);
+        connexionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(i, resultCode);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == 2)
+            finish();
     }
 }
---
+++ app/src/main/java/com/example/manon/snapclone/LoginActivity.java
@@ -0,0 +1,83 @@
+package com.example.manon.snapclone;
+
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText editUsername;
+    private EditText editPassword;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final Intent i = new Intent(this, MainActivity.class);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(editUsername.getText().toString().equals("maphdev") && editPassword.getText().toString().equals("lolipop")){
+                    setResult(2);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        });
+
+        editUsername = findViewById(R.id.login_username_edit);
+        editUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                checkColorEditText();
+            }
+        });
+
+        editPassword = findViewById(R.id.login_password_edit);
+        editPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                checkColorEditText();
+            }
+        });
+    }
+
+    private void checkColorEditText(){
+        if (!TextUtils.isEmpty(editUsername.getText().toString()) && !TextUtils.isEmpty(editPassword.getText().toString())) {
+            loginButton.setBackground(getResources().getDrawable(R.drawable.rounded_purple_button));
+        } else {
+            loginButton.setBackground(getResources().getDrawable(R.drawable.rounded_grey_button));
+        }
+    }
+}
